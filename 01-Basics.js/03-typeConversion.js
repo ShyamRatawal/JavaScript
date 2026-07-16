6de6f6ea692ca44361f6990db33d4f8ad0f59f55
Shyam-Ratawal <shyamratawal2@gmail.com>
2026-07-16 20:16:42 +0530

    add type conversion and operation

---
+++ 01-Basics.js/03-typeConversion.js
@@ -0,0 +1,30 @@
+let score = Number(4)
+let value = 10
+
+console.log(typeof score)
+
+let valueInScore = Number(score)
+let valueInScore = String(score)
+let valueInScore = Boolean(score)
+let valueInScore = Symbol(score)
+// Type Convertion of different datatypes 
+// Note: For conversion first letter of data Type should be capital 
+
+
+/* ========== OUTPUT ===========
+
+# From given data to number 
+"33" => 33
+"20abc" => NaN
+null => 0
+undefined => NaN
+"false" => 0
+"true" => 1
+
+#From given data to boolean
+1 => true
+0 => false
+"" => false
+"any string" => true
+
+*/
---
+++ 01-Basics.js/04-typeOperation.js
@@ -0,0 +1,35 @@
+let value = 5
+let negValue = -(value)
+// converts to negative value
+
+console.log(negValue)
+
+let str1 = "shyam"
+let str2 = " ratawal"
+
+let str3 = str1 + str2 
+// adds two strings 
+
+console.log(str3);
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + "2") // 12
+
+console.log("1" + 2 + 2) // 122
+// converts every element to the type of first on and adds the elements
+
+console.log(1 + 2 + "3"); // 33
+// adds 1 and 2, then adds it to 3 as string and prints
+
+console.log(true); // true
+
+console.log(+true) // 1
+
+console.log(""); // false
+console.log(+""); // 0
+
+let count = 10
+count++
+console.log(count) // 11
+
